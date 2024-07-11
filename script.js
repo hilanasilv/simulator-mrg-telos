610dd2588eae4aa20c6737d2ca42d1ac929ca154
Nayla Hilana <hilanacs@outlook.com>
2024-07-10 21:24:42 -0300

    implementando as histórias de usuário 3, 4 e 5

--- script.js
+++ script.js
@@ -21,15 +21,15 @@ const transacoesValidas = data.filter(transacao => {
 })
 
 //História de usuário 1 - Recebimento e Exibição de Dados do Governo
-console.log(`Total de repasses do governo: ${transacoesValidas.lenght}\n`);
+console.log(`Total de repasses do governo: ${transacoesValidas.length}\n`);
 
-const repasseSucesso = transacoesValidas.filter(data => data.status === "sucesso");
-const repasseFalha = transacoesValidas.filter(data => data.status === "falha");
+const repassesSucesso = transacoesValidas.filter(data => data.status === "sucesso");
+const repassesFalha = transacoesValidas.filter(data => data.status === "falha");
 
 /*História de usuário 2 - Análise de Transações por Status
 Parte I - Sucesso*/
-const totalRepassesComSucesso = repasseSucesso.lenght;
-const totalValorRepassesComSucesso = repasseSucesso.reduce((sum, data) => sum + data.valor, 0);
+const totalRepassesComSucesso = repassesSucesso.length;
+const totalValorRepassesComSucesso = repassesSucesso.reduce((sum, data) => sum + data.valor, 0);
 
 const repassesComSucessoPorOrgao = repassesSucesso.reduce((acc, data) => {
     if (!acc[data.orgao]) {
@@ -51,7 +51,7 @@ Object.keys(repassesComSucessoPorOrgao).forEach(orgao => {
 });
 
 //Parte II - Falhas
-const totalRepassesComFalha = repassesFalha.lenght;
+const totalRepassesComFalha = repassesFalha.length;
 const totalValorRepassesComFalha = repassesFalha.reduce((sum, data) => sum + data.valor, 0);
 
 const repassesComFalhaPorOrgao = repassesFalha.reduce((acc, data) => {
@@ -89,6 +89,75 @@ Object.keys(repassesComFalhaPorMotivo).forEach(motivo => {
     console.log(`Valor total de repasses: ${repassesComFalhaPorMotivo[motivo].valor}\n--------`);
 });
 
+//História de usuário 3 - Estatísticas de Repasses por critérios
+const repasseComMaiorValor = data.reduce((max, data) => data.valor > max.valor ? data : max, data[0]);
+const repasseComMenorValor = data.reduce((min, data) => data.valor < min.valor ? data : min, data[0]);
+
+const repassesPorDia = data.reduce((acc, data) => {
+    acc[data.data] = (acc[data.data] || 0) + 1;
+    return acc;
+});
+
+const diaRepasses = Object.keys(repassesPorDia).reduce((a, b) => repassesPorDia[a] > repassesPorDia[b] ? a : b);
+
+const repassesPorOrgao = data.reduce = data.reduce((acc, data) => {
+    acc[data.orgao] = (acc[data.orgao] || 0) + 1;
+    return acc;
+}, {});
+
+const orgaoComMaisRepasses = Object.keys(repassesPorOrgao).reduce((a, b) => repassesPorOrgao[a] > repassesPorOrgao[b] ? a : b);
+const orgaoMaisRepassesSucesso = Object.keys(repassesComSucessoPorOrgao).reduce((a, b) => repassesComSucessoPorOrgao[a].count > repassesComSucessoPorOrgao[b].count ? a : b);
+const orgaoMaisRepassesFalhados = Object.keys(repassesComFalhaPorOrgao).reduce((a, b) => repassesComFalhaPorOrgao[a].count > repassesComFalhaPorOrgao[b] ? a : b);
+const motivoMaisFalhas = Object.keys(repassesComFalhaPorMotivo).reduce((a, b) => repassesComFalhaPorMotivo[a].count > repassesComFalhaPorMotivo[b].count ? a : b);
+
+const detalhesRepasse = (repasse) => `
+    Orgão: ${repasse.orgao}
+    Data: ${repasse.data}
+    Valor: ${repasse.valor}
+    Motivo: ${repasse.motivo}
+`;
+
+//Exibir no console
+console.log("Detalhes do repasse com maior valor:", detalhesRepasse(repasseComMaiorValor));
+console.log("Detalhes do repasse com menor valor:", detalhesRepasse(repasseComMenorValor));
+console.log(`Data com mais repasses: ${diaRepasses}`);
+console.log(`Orgão com mais repasses: ${orgaoComMaisRepasses}`);
+console.log(`Orgão com mais repasses com sucesso: ${orgaoMaisRepassesSucesso}`);
+console.log(`Órgão com mais repasses falhados: ${orgaoMaisRepassesFalhados}`);
+console.log(`Motivo de falha com mais repasses: ${motivoMaisFalhas}`);
+
+//História de usuário 4 - Apresentação Automática de Detalhes de Transações
+const orgaoResponsavel = "Ministério da Saúde";
+
+const transacoesOrgao = data.filter(transacao => transacao.orgao === orgaoResponsavel);
+
+console.log(`\nDetalhes das transações do orgão: ${orgaoResponsavel}`);
+transacoesOrgao.forEach(transacao => {
+    console.log(`
+        Orgão: ${transacao.orgao}
+        Data: ${transacao.data}
+        Valor: ${transacao.valor}
+        Status: ${transacao.status}
+        Motivo: ${transacao.motivo || "o motivo não especificado"}
+    `);
+});
+
+//História de usuário 5 - Tratamento de erros
+const transacoesComProblema = transacoesValidas.filter(item => item.status === "falha" && !item.motivo);
+
+if(transacoesComProblema.length > 0){
+    console.log("\nErro de processamento!");
+    transacoesComProblema.forEach((transacao) => {
+        console.log("- Detalhes da transação:");
+        console.log(`  Orgão: ${transacao.orgao}`);
+        console.log(`  Data: ${transacao.data}`);
+        console.log(`  Valor: ${transacao.valor}`);
+        console.log(`  Status: ${transacao.status}`);
+        console.log("Erro: o motivo da falha de repasse não foi especificado.");
+    });
+} else{
+    console.log("Não foram encontradas transações com erro de processamento.");
+};
 
 
 
